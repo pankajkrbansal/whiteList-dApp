e4642be9e4375d15233cd805d0c496757819f25c
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-11-19 16:54:48 +0530

    Added basic whitelist contract

---
+++ blockchain/contracts/WhiteList.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity >0.8.0;
+
+contract WhiteList {
+    // mapping for users registered
+    mapping(address => bool) whiteListedMembers;
+    address owner;
+    uint256 maxWhiteListLimit;
+    uint256 whiteListCount;
+
+    modifier onlyOwner(address addr) {
+        require(addr == owner, "Only Owner Can Call This Function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setWhiteListLimit(uint256 limit) external onlyOwner(msg.sender) {
+        maxWhiteListLimit = limit;
+    }
+
+    function addSendertoWhitelist() external{
+        require(whiteListCount < maxWhiteListLimit, "List Is Full, Better Luck Next Time");
+        require(whiteListedMembers[msg.sender], "Member With This Address Is Already Registered");
+
+        // adding member to white list
+        whiteListedMembers[msg.sender] = true;
+        // Increasing the count of current count;
+        whiteListCount = whiteListCount + 1;
+    }
+}
