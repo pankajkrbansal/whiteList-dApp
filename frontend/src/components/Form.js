abbc88be0c698d53c88d8aec1043f74506d399eb
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-11-25 01:05:55 +0530

    finished app

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,46 +1,58 @@
 import { React, useEffect, useState } from "react";
 import abi from "../abi.json";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 // require('dotenv').config();
 
 export default function Form(data) {
   const [addrPresent, setAddrPresent] = useState(false);
   const contractABI = abi.abi;
-  console.log(data);
-  console.log(data.account);
 
-  useEffect(()=>{
-    checkAddressInWitelist(data.account)
+  useEffect(() => {
+    console.log(data);
+    checkAddressInWitelist(data.account);
     console.log("Loaded");
-  },[]);
+  }, []);
 
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
-  const contractInstance = new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS, contractABI, signer);
+  // console.log(signer);
+  const contractInstance = new ethers.Contract(
+    process.env.REACT_APP_CONTRACT_ADDRESS,
+    contractABI,
+    signer
+  );
 
-  const addToWhiteList = async () => {};
+  const addToWhiteList = async () => {
+    let res = await contractInstance.whiteListedMembers(data.account);
+    if (!res) {
+      await contractInstance.addSendertoWhitelist();
+      setAddrPresent(true);
+    }
+  };
 
-  const checkAddressInWitelist = async(address) => {
-    const signer = provider.getSigner();
-    console.log(signer);
-    // await contractInstance.
+  const checkAddressInWitelist = async (address) => {
+    let res = await contractInstance.whiteListedMembers(address);
+    console.log(res);
+    setAddrPresent(res);
   };
 
   const isWalletConnected = data.isWalletConnected;
 
-  if (isWalletConnected) {
+  if (isWalletConnected && !addrPresent) {
     return (
       <div>
-          {/* <input className="addr" name="addr" placeholder="Enter Address Here"></input> <br/><br/> */}
-          <button className="button" disabled={addrPresent} onClick={addToWhiteList}>Add To WhiteList</button>
+        {/* <input className="addr" name="addr" placeholder="Enter Address Here"></input> <br/><br/> */}
+        <button
+          className="button"
+          disabled={addrPresent}
+          onClick={addToWhiteList}
+        >
+          Add To WhiteList
+        </button>
       </div>
     );
   } else {
     // add text for address already in whitelist
-    return(
-      <h3>
-        Address Is Already Added In The WhiteList.
-      </h3>
-    )
+    return <h3>Address Is Added In The WhiteList.</h3>;
   }
 }
